87f83b035b33d958863f50762e64fd242c98beec
Tom Shafer <tom@tshafer.com>
2020-06-04 07:37:37 -0400

    Add WK1 experiments

--- src/nlp/models.py
+++ src/nlp/models.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch.nn import functional as F
 
 __all__ = ["BOWClassifier", "CBOWClassifier"]
 
@@ -30,12 +31,55 @@ class CBOWClassifier(nn.Module):
         self.embed = nn.Embedding(vocab_size, embedding_size, padding_idx=0,)
         nn.init.xavier_uniform_(self.embed.weight)
 
-        self.dropout = nn.Dropout(p=dropout)
-
         self.softmax = nn.Linear(in_features=embedding_size, out_features=class_size)
-        nn.init.xavier_uniform_(self.softmax.weight)  # Weight inits from sample
+        nn.init.xavier_uniform_(self.softmax.weight)
+        self.dropout2 = nn.Dropout(p=dropout)
 
     def forward(self, input):
         y = self.embed(input).sum(dim=1)
-        y = self.dropout(y)
-        return self.softmax(y)
+        return self.softmax(self.dropout2(y))
+
+
+class DeepCBOWClassifier(nn.Module):
+    """A "deep" CBoW classifier adds a nonlinearity to the CBoW classifier."""
+
+    def __init__(
+        self,
+        vocab_size: int,
+        class_size: int,
+        embedding_size: int = 64,
+        hidden_size: int = 64,
+        dropout: float = 0,
+    ):
+        super().__init__()
+
+        # Embeddings
+        self.embed = nn.Embedding(vocab_size, embedding_size, padding_idx=0,)
+        nn.init.xavier_uniform_(self.embed.weight)
+        # self.dropout_embed = nn.Dropout(p=dropout)
+
+        # Hidden layer; cf sample code
+        self.hidden = nn.ModuleList(
+            [
+                nn.Linear(in_features=embedding_size, out_features=hidden_size),
+                nn.Dropout(p=dropout),
+                nn.ReLU(),
+                nn.Linear(in_features=hidden_size, out_features=hidden_size),
+                nn.Dropout(p=dropout),
+                nn.ReLU(),
+            ]
+        )
+        nn.init.xavier_uniform_(self.hidden[0].weight)
+        nn.init.xavier_uniform_(self.hidden[3].weight)
+
+        # Softmax
+        self.softmax = nn.Linear(in_features=hidden_size, out_features=class_size)
+        nn.init.xavier_uniform_(self.softmax.weight)
+        self.dropout_softmax = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        y = self.embed(x).sum(dim=1)
+        for layer in self.hidden:
+            y = layer(y)
+        y = self.dropout_softmax(self.softmax(y))
+        return y
